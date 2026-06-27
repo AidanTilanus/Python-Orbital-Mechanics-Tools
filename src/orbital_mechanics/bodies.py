1bafdea2d60c442f0ce50c061dc87228b94dcc2c
AidanTilanus <Aidan.tilanus@gmail.com>
2026-06-27 13:37:41 +0200

    added geostationary_orbit_radius to Body

--- src/orbital_mechanics/bodies.py
+++ src/orbital_mechanics/bodies.py
@@ -1,16 +1,19 @@
 from pint import Quantity
+from numpy import pi
 
 from .units import u
 from .constants import G
 from .utils import require_dimension
 
 class Body:
-    def __init__(self, mass: Quantity, radius: Quantity):
+    def __init__(self, mass: Quantity, radius: Quantity, rotation_period: Quantity = 0 * u.s):
         require_dimension(mass, "[mass]", "mass")
         require_dimension(radius, "[length]", "radius")
+        require_dimension(rotation_period, "[time]", "rotation_period")
 
         self.mass = mass
         self.radius = radius
+        self.rotation_period = rotation_period
     
     def __repr__(self) -> str:
         return f"Body(mass={self.mass}, radius={self.radius})"
@@ -18,6 +21,12 @@ class Body:
     @property
     def gravitational_parameter(self) -> Quantity:
         return G * self.mass
+    
+    @property
+    def geostationary_orbit_radius(self) -> Quantity:
+        if self.rotation_period == 0 * u.s:
+            raise ValueError("Body has no rotation period, cannot calculate geostationary orbit radius.")
+        return (self.gravitational_parameter * self.rotation_period**2 / (4 * pi**2))**(1/3)
     
     def altitude_to_r(self, altitude: Quantity) -> Quantity:
         require_dimension(altitude, "[length]", "altitude")
--- src/orbital_mechanics/solar_system.py
+++ src/orbital_mechanics/solar_system.py
@@ -1,12 +1,21 @@
 from .units import u
 from .bodies import Body
 
-MERCURY = Body(mass=3.3011e23 * u.kg, radius=2439.7 * u.km)
-VENUS   = Body(mass=4.8675e24 * u.kg, radius=6051.8 * u.km)
-EARTH   = Body(mass=5.9724e24 * u.kg, radius=6378.137 * u.km)
-MOON    = Body(mass=7.342e22  * u.kg, radius=1737.4 * u.km)
-MARS    = Body(mass=6.4171e23 * u.kg, radius=3396.2 * u.km)
-JUPITER = Body(mass=1.8982e27 * u.kg, radius=71492 * u.km)
-SATURN  = Body(mass=5.6834e26 * u.kg, radius=60268 * u.km)
-URANUS  = Body(mass=8.6810e25 * u.kg, radius=25559 * u.km)
-NEPTUNE = Body(mass=1.02413e26 * u.kg, radius=24764 * u.km)
+MERCURY = Body(mass=3.3011e23 * u.kg, radius=2439.7 * u.km,
+                rotation_period=5_067_031.68 * u.s)   # 58.6462 days
+VENUS   = Body(mass=4.8675e24 * u.kg, radius=6051.8 * u.km,
+                rotation_period=20_997_152.64 * u.s)  # 243.0226 days, retrograde
+EARTH   = Body(mass=5.9724e24 * u.kg, radius=6378.137 * u.km,
+                rotation_period=86164.0905 * u.s)     # sidereal day
+MOON    = Body(mass=7.342e22  * u.kg, radius=1737.4 * u.km,
+                rotation_period=2_360_591.5 * u.s)    # 27.321661 days, tidally locked
+MARS    = Body(mass=6.4171e23 * u.kg, radius=3396.2 * u.km,
+                rotation_period=88_642.66 * u.s)      # 1.025957 days
+JUPITER = Body(mass=1.8982e27 * u.kg, radius=71492 * u.km,
+                rotation_period=35_730 * u.s)         # 9h 55m 30s
+SATURN  = Body(mass=5.6834e26 * u.kg, radius=60268 * u.km,
+                rotation_period=38_018 * u.s)         # 10h 33m 38s
+URANUS  = Body(mass=8.6810e25 * u.kg, radius=25559 * u.km,
+                rotation_period=62_064 * u.s)         # 17h 14m, retrograde
+NEPTUNE = Body(mass=1.02413e26 * u.kg, radius=24764 * u.km,
+                rotation_period=57_996 * u.s)         # 16h 6m
