6e0730abbfa3242fc4fbc23a4b1df09154479cee
AidanTilanus <Aidan.tilanus@gmail.com>
2026-06-27 11:41:31 +0200

    moved package to src/orbital_mechanics

--- src/orbital_mechanics/__init__.py
+++ src/orbital_mechanics/__init__.py
@@ -3,6 +3,7 @@ from .rockets import Stage, Rocket
 from .bodies import Body
 from .solar_system import MERCURY, VENUS, EARTH, MOON, MARS, JUPITER, SATURN, URANUS, NEPTUNE
 from .maneuvers import BurnVector
+from .elements import semi_major_axis, eccentricity, vis_viva, circular_orbit_speed, escape_velocity, orbital_period
 
 __all__ = [
     "G",  "GRAVITANTIONAL_CONSTANT",
@@ -10,5 +11,6 @@ __all__ = [
     "Stage", "Rocket",
     "Body",
     "MERCURY", "VENUS", "EARTH", "MOON", "MARS", "JUPITER", "SATURN", "URANUS", "NEPTUNE",
-    "BurnVector"
+    "BurnVector",
+    "semi_major_axis", "eccentricity", "vis_viva", "circular_orbit_speed", "escape_velocity", "orbital_period"
 ]
