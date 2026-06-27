99fcdfd065253fa5091fee95f001cb1ac50e94ab
AidanTilanus <Aidan.tilanus@gmail.com>
2026-06-27 14:50:00 +0200

    fixed apo and peri to be the radius

--- src/orbital_mechanics/orbit.py
+++ src/orbital_mechanics/orbit.py
@@ -4,17 +4,17 @@ from .units import u
 from .utils import require_dimension
 
 class Orbit:
-    def __init__(self, apo: Quantity, peri: Quantity):
-        require_dimension(apo,  "[length]", "apoapsis")
-        require_dimension(peri, "[length]", "periapsis")
+    def __init__(self, r_apo: Quantity, r_peri: Quantity):
+        require_dimension(r_apo,  "[length]", "r_apoapsis")
+        require_dimension(r_peri, "[length]", "r_periapsis")
         
-        self.apo  = apo
-        self.peri = peri
+        self.r_apo  = r_apo
+        self.r_peri = r_peri
         
     @property
     def semi_major_axis(self) -> Quantity:
-        return (self.apo + self.peri) / 2
+        return (self.r_apo + self.r_peri) / 2
     
     @property
     def eccentricity(self) -> Quantity:
-        return (self.apo - self.peri) / (self.apo + self.peri)
+        return (self.r_apo - self.r_peri) / (self.r_apo + self.r_peri)
