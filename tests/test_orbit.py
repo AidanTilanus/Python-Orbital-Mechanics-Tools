27a22d17dada14c8996b9f2623c2cd1e9cb07162
AidanTilanus <Aidan.tilanus@gmail.com>
2026-06-27 13:55:40 +0200

    Update test_orbit.py

--- tests/test_orbit.py
+++ tests/test_orbit.py
@@ -1,14 +1,14 @@
 import orbital_mechanics as om
 
 def test_orbit_creation():
-    orbit = om.Orbit(100 * u.km, 80 * u.km)
+    orbit = om.Orbit(100 * om.u.km, 80 * om.u.km)
     assert orbit.apo == 100
     assert orbit.peri == 80
 
 def test_body_semi_major_axis():
-    orbit = om.Orbit(100 * u.km, 80 * u.km)
-    assert orbit.semi_major_axis == ((100 + 80) / 2) * u.km
+    orbit = om.Orbit(100 * om.u.km, 80 * om.u.km)
+    assert orbit.semi_major_axis == ((100 + 80) / 2) * om.u.km
 
 def test_body_eccentricity():
-    orbit = om.Orbit(100 * u.km, 80 * u.km)
-    assert orbit.eccentricity == (100 - 20) / (100 + 80) * u.km
+    orbit = om.Orbit(100 * om.u.km, 80 * om.u.km)
+    assert orbit.eccentricity == (100 - 20) / (100 + 80) * om.u.km
