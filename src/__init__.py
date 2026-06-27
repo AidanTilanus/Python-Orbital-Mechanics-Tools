c404496e4abdf5e0a6d33fc6bbb70a4b08e7acf2
AidanTilanus <Aidan.tilanus@gmail.com>
2026-06-27 10:46:47 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,14 @@
 from .constants import G, GRAVITANTIONAL_CONSTANT, G0, standard_gravity
+from .rockets import Stage, Rocket
+from .bodies import Body
+from .solar_system import MERCURY, VENUS, EARTH, MOON, MARS, JUPITER, SATURN, URANUS, NEPTUNE
+from .maneuvers import BurnVector
 
 __all__ = [
     "G",  "GRAVITANTIONAL_CONSTANT",
-    "G0", "standard_gravity"
+    "G0", "standard_gravity",
+    "Stage", "Rocket",
+    "Body",
+    "MERCURY", "VENUS", "EARTH", "MOON", "MARS", "JUPITER", "SATURN", "URANUS", "NEPTUNE",
+    "BurnVector"
 ]
